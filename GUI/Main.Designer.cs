e30fd6f778341bc4083422eccf0bc169a9974ddc
kadzicu <kadzicuh@gmail.com>
2021-01-29 13:38:06 +0300

    Add files via upload

---
+++ GUI/Main.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace GUI
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.removeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(2, 2);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(562, 150);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "File";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.removeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(118, 26);
+            // 
+            // removeToolStripMenuItem
+            // 
+            this.removeToolStripMenuItem.Name = "removeToolStripMenuItem";
+            this.removeToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.removeToolStripMenuItem.Text = "Remove";
+            this.removeToolStripMenuItem.Click += new System.EventHandler(this.RemoveToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(2, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(562, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Build";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Main
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(566, 193);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Joiner";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Main_DragDrop);
+            this.DragOver += new System.Windows.Forms.DragEventHandler(this.Main_DragOver);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ GUI/Main.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Main : Form
+    {
+        private readonly string _Startup_Path = Application.StartupPath + "\\Temp";
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] _Files = e.Data.GetData(DataFormats.FileDrop) as string[];
+            string _Path = null;
+
+            foreach (string _File in _Files)
+                _Path = Path.GetFullPath(_File);
+
+            ListViewItem _List_View_Item = new ListViewItem
+            {
+                Text = _Path
+            };
+
+            listView1.Items.Insert(0x0, _List_View_Item);
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            listView1.AutoResizeColumn(0x1, ColumnHeaderAutoResizeStyle.ColumnContent);
+        }
+
+        private void Main_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void RemoveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0x0)
+                foreach (ListViewItem _Item in listView1.SelectedItems)
+                    _Item.Remove();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog _Save_File_Dialog = new SaveFileDialog())
+            {
+                _Save_File_Dialog.Filter = "Executable (*.exe)|*.exe";
+                _Save_File_Dialog.ShowDialog();
+
+                Directory.CreateDirectory(_Startup_Path);
+                string _Files = "";
+
+                for (int i = 0x0; i < listView1.Items.Count; i++)
+                {
+                    string _New_File_Name = listView1.Items[i].SubItems[0x0].Text.Remove(0x0, listView1.Items[i].SubItems[0x0].Text.LastIndexOf('\\') + 0x1).Replace(" ", string.Empty);
+                    string _New_Path = _Startup_Path + "\\" + _New_File_Name;
+
+                    File.Copy(listView1.Items[i].SubItems[0x0].Text, _New_Path);
+
+                    _Files += string.Format("Section \"{0}\"\r\nSetOutPath \"${1}\"\r\nSetOverwrite on\r\nFile \"{2}\"\r\nExec ${3}\\{4}\r\nSectionEnd\r\n\r\n", "file" + i, "Temp", _New_Path, "Temp", _New_Path.Remove(0x0, _New_Path.LastIndexOf('\\') + 0x1));
+                }
+
+                File.AppendAllText("source.txt", "OutFile \"out.exe\"\r\nSilentInstall silent\r\nRequestExecutionLevel user\r\n" + _Files);
+                Make();
+                File.Copy(Application.StartupPath + "\\out.exe", _Save_File_Dialog.FileName);
+                Directory.Delete(_Startup_Path, true);
+                File.Delete("source.txt");
+                File.Delete("out.exe");
+                MessageBox.Show("Done");
+            }
+        }
+
+        private void Make()
+        {
+            using (Process _Process = new Process())
+            {
+                _Process.StartInfo.CreateNoWindow = true;
+                _Process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                _Process.StartInfo.FileName = Application.StartupPath + "\\NSIS\\makensis.exe";
+                _Process.StartInfo.UseShellExecute = false;
+                _Process.StartInfo.RedirectStandardOutput = true;
+                _Process.StartInfo.Arguments = "source.txt";
+                _Process.Start();
+                _Process.BeginOutputReadLine();
+                _Process.WaitForExit();
+            }
+        }
+    }
+}
